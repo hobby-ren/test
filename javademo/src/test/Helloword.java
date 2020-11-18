c37e4b7496e8807b5fca279f34833fc6db05eced
admin <admin@qq.com>
2020-11-18 10:44:41 +0800

    这是我的第一次提交代码

---
+++ javademo/src/test/Helloword.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//打印一个helloword！！！
+		System.out.println("hello word!!!!!");
+	}
+
+}
